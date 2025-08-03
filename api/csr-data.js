f58608a5d1f4e185d805e9376bda2759620bffaa
athanikonda <ashok.thanikonda@gmail.com>
2025-08-03 22:20:25 +0530

    Add files via upload

---
+++ api/csr-data.js
@@ -0,0 +1,14 @@
+// api/csr-data.js
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  const SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRaDCGxkQyoqBF6_genJT1KztlWoeY8cNLMlIRSlSKSvRLidz_449ZFzbrO0sCQFf9HGiYdySFa8weC/pub?output=csv';
+  try {
+    const response = await axios.get(SHEET_URL);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'text/csv');
+    res.send(response.data);
+  } catch {
+    res.status(500).send('Failed to fetch CSV');
+  }
+};
